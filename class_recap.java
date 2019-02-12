a8c3fb2c0bee706c8341c00a73fdcd471078a278
Chan Jung <ckse93@gmail.com>
2019-02-12 16:08:43 -0600

    Create class_recap.java
    just revisiting java every once in a while

---
+++ class_recap.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        class User{
+            int score;
+            String id;
+            public boolean hasWon () {
+                if (score == 100) {
+                    return true;
+                }
+                return false;
+            }
+            public int getScore(){return this.score;}
+            public String getId(){return this.id;}
+            public void setScore(int score){
+                this.score = score;
+            }
+            public void setId(String id){
+                this.id = id;
+            }
+        }
+
+
+        User user1 = new User();
+        user1.setId("ckse93");
+        user1.setScore(99);
+        System.out.println(user1.hasWon());
+    }
+}
